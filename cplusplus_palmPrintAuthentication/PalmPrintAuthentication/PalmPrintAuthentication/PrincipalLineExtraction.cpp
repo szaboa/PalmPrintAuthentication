8252e239705f9422faa5a287dea3a9806d1fd5ff
Arnold Szabo <szaboaarnold@gmail.com>
2016-02-22 19:50:07 +0200

    implemented directional principal line detection

--- cplusplus_palmPrintAuthentication/PalmPrintAuthentication/PalmPrintAuthentication/PrincipalLineExtraction.cpp
+++ cplusplus_palmPrintAuthentication/PalmPrintAuthentication/PalmPrintAuthentication/PrincipalLineExtraction.cpp
@@ -8,34 +8,198 @@ PrincipalLineExtraction::PrincipalLineExtraction(Mat roi) : roi(roi)
 {
 	// Convert to grayscale
 	cvtColor(roi, this->roi, CV_BGR2GRAY);
-	resize(this->roi, this->roi, Size(128, 128));
 
+	// Resize it to 128x128
+	resize(this->roi, this->roi, Size(roiWidth, roiHeight));
+
+	// Show original ROI
 	namedWindow("Before normalization", CV_WINDOW_AUTOSIZE);
 	imshow("Before normalization", this->roi);
 
-	// Image normalization
+	// Image normalization (using Histogram Equalization and a Low-pass Filter)
 	normalizeImage(this->roi);
 
+	// Show ROI after normalization
 	namedWindow("After normalization", CV_WINDOW_AUTOSIZE);
 	imshow("After normalization", this->roi);
 
-	// Locate principal lines in four directions, then merge them
-	locatePrincipalLines(this->roi);
+	// Locate principal lines
+	locatePrincipalLines();
 
 	waitKey(0);
-
 }
 
 void PrincipalLineExtraction::normalizeImage(Mat &img){
 	equalizeHist(img, img);
 	blur(img, img, Size(5, 5), Point(-1, -1));
 }
-void PrincipalLineExtraction::locatePrincipalLines(Mat &img){
 
-	// defining horizontal line detecors
+Mat PrincipalLineExtraction::locatePrincipalLineInGivenDirection(const Mat &img, std::vector<std::vector<double>> H1, std::vector<std::vector<double>> H2, int degree){
 
-	// 1-D Gaussian function x first-order derivative of 1-D Gaussian function
-	double H1[5][9] = {
+
+	int H1_rows = H1.size();
+	int H1_cols = H1[0].size();
+
+
+	// First derivative of the input image
+	double I_der[roiHeight][roiWidth];
+
+	// Second derivate of the input image
+	double I_der2[roiHeight][roiWidth];
+
+
+	// Initialize first- and second-order derivatives
+	for (int i = 0; i < roiHeight; ++i){
+		for (int j = 0; j < roiWidth; ++j){
+			I_der[i][j]  = 0.0;
+			I_der2[i][j] = 0.0;
+		}
+	}
+
+	// Calculate first- and second-order derivatives
+	for (int i = H1_rows / 2; i < roiHeight - (H1_rows / 2); ++i){
+		for (int j = H1_cols / 2; j < roiWidth - (H1_cols / 2); ++j){
+
+			for (int _i = -(H1_rows / 2); _i <= (H1_rows / 2); ++_i){
+				for (int _j = -(H1_cols / 2); _j <= +(H1_cols / 2); ++_j){
+					I_der[i][j]  += static_cast<int>(img.at<uchar>(i + _i, j + _j)) * H1[_i + (H1_rows / 2)][_j + (H1_cols / 2)];
+					I_der2[i][j] += static_cast<int>(img.at<uchar>(i + _i, j + _j)) * H2[_i + (H1_rows / 2)][_j + (H1_cols / 2)];
+				}
+			}
+		}
+	}
+
+	// Create binary image, this will contain the extracted principal lines in a given direction
+	Mat binaryImage(this->roi.size(), CV_8U);
+	
+	// Locating changes in first-order derivatives in 0 direction
+	if (degree == 0){
+
+		// Traversing the matrix column by column
+		for (int j = 3; j < 124; ++j){
+			for (int i = 1; i < 126; ++i){
+
+				bool derivChangeFound = false;
+
+				// Checking if the derivative's sign has changed by comparing #dw neighbors 
+				for (int dw = 1; dw < firstDerivChangeWidth; ++dw){
+					if (isInsideTheBoundary(i - dw, j)){
+						if ((I_der[i][j] * I_der[i-dw][j]) < 0){
+							derivChangeFound = true;
+						}
+					}
+				}
+				/* If first-order derivative's sign has changed and the second-order derivative's value is greater 
+				   than the treshold, then setting the current pixel to 255, otherwise to 0 */
+				if ((derivChangeFound || I_der[i][j] == 0) && I_der2[i][j] > secondDerivTresholdValue){
+					binaryImage.at<uchar>(i, j) = 255;
+				}
+				else{
+					binaryImage.at<uchar>(i, j) = 0;
+				}
+			}
+		}
+	}
+
+	// Locating changes in first-order derivatives in 90 direction
+	if (degree == 90){
+
+		// Traversing the matrix row by row
+		for (int i = 1; i < 126; ++i){
+			for (int j = 3; j < 124; ++j){
+
+				bool derivChangeFound = false;
+				// Checking if the derivative's sign has changed by comparing #dw neighbors 
+				for (int dw = 1; dw < firstDerivChangeWidth; ++dw){
+					if (isInsideTheBoundary(i, j - dw)){
+						if ((I_der[i][j] * I_der[i][j - dw]) < 0){
+							derivChangeFound = true;
+						}
+					}
+				}
+				/* If first-order derivative's sign has changed and the second-order derivative's value is greater
+				than the treshold, then setting the current pixel to 255, otherwise to 0 */
+				if ((derivChangeFound || I_der[i][j] == 0) && I_der2[i][j] > secondDerivTresholdValue){
+					binaryImage.at<uchar>(i, j) = 255;
+				}
+				else{
+					binaryImage.at<uchar>(i, j) = 0;
+				}
+			}
+		}
+	}
+
+
+	// Locating changes in first-order derivatives in 45 direction
+	if (degree == 45){
+
+		// Traversing the matrix diagonal (bottom-left to top-right)
+		for (int i = 1; i < 2 * 128 - 2; ++i){
+			int k = i < 128 ? 0 : i - 128 + 1;
+			for (int j = k+1; j <= i - k; ++j){
+
+				bool derivChangeFound = false;
+				// Checking if the derivative's sign has changed by comparing #dw neighbors 
+				for (int dw = 1; dw < firstDerivChangeWidth; ++dw){
+					if (isInsideTheBoundary(j - dw, i - j)){
+						if ((I_der[j][i - j] * I_der[j - dw][i - j]) < 0){
+							derivChangeFound = true;
+						}
+					}
+				}
+				/* If first-order derivative's sign has changed and the second-order derivative's value is greater
+				than the treshold, then setting the current pixel to 255, otherwise to 0 */
+				if ((derivChangeFound || I_der[j][i - j] == 0) && I_der2[j][i - j] > secondDerivTresholdValue){
+					binaryImage.at<uchar>(j, i-j) = 255;
+				}
+				else{
+					binaryImage.at<uchar>(j, i - j) = 0;
+				}	
+			}
+		}
+	}
+
+	// Locating changes in first-order derivatives in 45 direction
+	if (degree == 135){
+
+		// Traversing the matrix diagonal (bottom-right to top-left)
+		for (int i = 1; i < 2 * 128 - 2; ++i){
+			int k = i < 128 ? 0 : i - 128 + 1;
+			for (int j = k + 1; j <= i - k; ++j){
+
+				int index = (128 - 1) - (i - j);
+				bool derivChangeFound = false;
+				// Checking if the derivative's sign has changed by comparing #dw neighbors 
+				for (int dw = 1; dw < firstDerivChangeWidth; ++dw){
+					if (isInsideTheBoundary(j - dw, index)){
+						if ((I_der[j][index] * I_der[j - dw][index]) < 0){
+							derivChangeFound = true;
+						}
+					}
+				}
+
+				/* If first-order derivative's sign has changed and the second-order derivative's value is greater
+				than the treshold, then setting the current pixel to 255, otherwise to 0 */
+				if ((derivChangeFound || I_der[j][index] == 0) && I_der2[j][index] > secondDerivTresholdValue){
+					binaryImage.at<uchar>(j, index) = 255;
+				}
+				else{
+					binaryImage.at<uchar>(j, index) = 0;
+				}
+			}
+		}
+	}
+	return binaryImage;
+}
+
+void PrincipalLineExtraction::locatePrincipalLines(){
+
+
+	// (1-D Gaussian function) x (first-order derivative of 1-D Gaussian function) 
+
+	// Directional line detector in 0 direction
+	vector<vector<double>> H1_0 = 
+	{
 		{ 0.0009, 0.0027, 0.0058, 0.0092, 0.0107, 0.0092, 0.0058, 0.0027, 0.0009 },
 		{ 0.0065, 0.0191, 0.0412, 0.0655, 0.0764, 0.0655, 0.0412, 0.0191, 0.0065 },
 		{ 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000 },
@@ -43,17 +207,52 @@ void PrincipalLineExtraction::locatePrincipalLines(Mat &img){
 		{ -0.0065, -0.0191, -0.0412, -0.0655, -0.0764, -0.0655, -0.0412, -0.0191, -0.0065 }
 	};
 
-	double H1_45[5][9] = {
-		{ 0.0009, 0.0027, 0.0058, 0.0092, 0.0107, 0.0092, 0.0058, 0.0027, 0.0009 },
-		{ 0.0065, 0.0191, 0.0412, 0.0655, 0.0764, 0.0655, 0.0412, 0.0191, 0.0065 },
-		{ 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000 },
-		{ -0.0009, -0.0027, -0.0058, -0.0092, -0.0107, -0.0092, -0.0058, -0.0027, -0.0009 },
-		{ -0.0065, -0.0191, -0.0412, -0.0655, -0.0764, -0.0655, -0.0412, -0.0191, -0.0065 }
+	// Directional line detector in 45 direction 
+	vector<vector<double>> H1_45 = 
+	{ 
+		{ -0.000900, -0.006500, 0.000000, 0.006500, 0.000900, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, -0.002700, -0.019100, 0.000000, 0.019100, 0.002700, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, -0.005800, -0.041200, 0.000000, 0.041200, 0.005800, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, -0.009200, -0.065500, 0.000000, 0.065500, 0.009200, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, -0.010700, -0.076400, 0.000000, 0.076400, 0.010700, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, -0.009200, -0.065500, 0.000000, 0.065500, 0.009200, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, -0.005800, -0.041200, 0.000000, 0.041200, 0.005800, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, -0.002700, -0.019100, 0.000000, 0.019100, 0.002700, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, -0.000900, -0.006500, 0.000000, 0.006500, 0.000900 }
 	};
 
+	// Directional line detector in 90 direction
+	vector<vector<double>> H1_90 = 
+	{ 
+		{ -0.000900, -0.006500, 0.0, 0.006500, 0.000900 },
+		{ -0.002700, -0.019100, 0.0, 0.019100, 0.002700 },
+		{ -0.005800, -0.041200, 0.0, 0.041200, 0.005800 },
+		{ -0.009200, -0.065500, 0.0, 0.065500, 0.009200 },
+		{ -0.010700, -0.076400, 0.0, 0.076400, 0.010700 },
+		{ -0.009200, -0.065500, 0.0, 0.065500, 0.009200 },
+		{ -0.005800, -0.041200, 0.0, 0.041200, 0.005800 },
+		{ -0.002700, -0.019100, 0.0, 0.019100, 0.002700 },
+		{ -0.000900, -0.006500, 0.0, 0.006500, 0.000900 }
+	};
+
+	// Directional line detector in 135 direction
+	vector<vector<double>> H1_135 = 
+	{ 
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, -0.000900, -0.006500, 0.000000, 0.006500, 0.000900 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, -0.002700, -0.019100, 0.000000, 0.019100, 0.002700, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, -0.005800, -0.041200, 0.000000, 0.041200, 0.005800, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, -0.009200, -0.065500, 0.000000, 0.065500, 0.009200, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, -0.010700, -0.076400, 0.000000, 0.076400, 0.010700, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, -0.009200, -0.065500, 0.000000, 0.065500, 0.009200, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, -0.005800, -0.041200, 0.000000, 0.041200, 0.005800, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, -0.002700, -0.019100, 0.000000, 0.019100, 0.002700, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ -0.000900, -0.006500, 0.000000, 0.006500, 0.000900, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 }
+	};
 
 	// 1-D Gaussian function x second-order derivative of 1-D Gaussian function
-	double H2[5][9] = {
+
+	// Directional line detector in 0 direction
+	vector<vector<double>> H2_0 = { 
 		{ 0.0156, 0.0211, 0.0309, 0.0416, 0.0464, 0.0416, 0.0309, 0.0211, 0.0156 },
 		{ 0.0257, 0.0510, 0.0954, 0.1441, 0.1660, 0.1441, 0.0954, 0.0510, 0.0257 },
 		{ -0.0298, -0.1125, -0.2582, -0.4178, -0.4896, -0.4178, -0.2582, -0.1125, -0.0298 },
@@ -61,67 +260,68 @@ void PrincipalLineExtraction::locatePrincipalLines(Mat &img){
 		{ 0.0156, 0.0211, 0.0309, 0.0416, 0.0464, 0.0416, 0.0309, 0.0211, 0.0156 }
 	};
 
-	// First derivative of the input image
-	double I_der[128][128];
+	// Directional line detector in 45 direction
+	vector<vector<double>> H2_45 = 
+	{ 
+		{ 0.015600, 0.025700, -0.029800, 0.025700, 0.015600, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.021100, 0.051000, -0.112500, 0.051000, 0.021100, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.030900, 0.095400, -0.258200, 0.095400, 0.030900, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.041600, 0.144100, -0.417800, 0.144100, 0.041600, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.046400, 0.166000, -0.489600, 0.166000, 0.046400, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.041600, 0.144100, -0.417800, 0.144100, 0.041600, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.030900, 0.095400, -0.258200, 0.095400, 0.030900, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.021100, 0.051000, -0.112500, 0.051000, 0.021100, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.015600, 0.025700, -0.029800, 0.025700, 0.015600 }
+	};
 
-	// Second derivate of the input image
-	double I_der2[128][128];
+	// Directional line detector in 90 direction
+	vector<vector<double>> H2_90 = 
+	{ 
+		{ 0.015600, 0.025700, -0.029800, 0.025700, 0.015600 },
+		{ 0.021100, 0.051000, -0.112500, 0.051000, 0.021100 },
+		{ 0.030900, 0.095400, -0.258200, 0.095400, 0.030900 },
+		{ 0.041600, 0.144100, -0.417800, 0.144100, 0.041600 },
+		{ 0.046400, 0.166000, -0.489600, 0.166000, 0.046400 },
+		{ 0.041600, 0.144100, -0.417800, 0.144100, 0.041600 },
+		{ 0.030900, 0.095400, -0.258200, 0.095400, 0.030900 },
+		{ 0.021100, 0.051000, -0.112500, 0.051000, 0.021100 },
+		{ 0.015600, 0.025700, -0.029800, 0.025700, 0.015600 }
+	};
 
+	// Directional line detector in 135 direction
+	vector<vector<double>> H2_135 = 
+	{ 
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.015600, 0.025700, -0.029800, 0.025700, 0.015600 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.021100, 0.051000, -0.112500, 0.051000, 0.021100, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.030900, 0.095400, -0.258200, 0.095400, 0.030900, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.041600, 0.144100, -0.417800, 0.144100, 0.041600, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.000000, 0.046400, 0.166000, -0.489600, 0.166000, 0.046400, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.000000, 0.041600, 0.144100, -0.417800, 0.144100, 0.041600, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.000000, 0.030900, 0.095400, -0.258200, 0.095400, 0.030900, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.000000, 0.021100, 0.051000, -0.112500, 0.051000, 0.021100, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 },
+		{ 0.015600, 0.025700, -0.029800, 0.025700, 0.015600, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000 }
+	};
 
-	// Initialize first- and second-order derivatives
-	for (int i = 0; i < 128; ++i){
-		for (int j = 0; j < 128; ++j){
-			I_der[i][j]  = 0.0;
-			I_der2[i][j] = 0.0;
-		}
-	}
+	// Locating principal lines in four directions
+	Mat linesInDir0   = locatePrincipalLineInGivenDirection(this->roi, H1_0, H2_0, 0);
+	Mat linesInDir90  = locatePrincipalLineInGivenDirection(this->roi, H1_90, H2_90, 90);
+	Mat linesInDir45  = locatePrincipalLineInGivenDirection(this->roi, H1_45, H2_45, 45);
+	Mat linesInDir135 = locatePrincipalLineInGivenDirection(this->roi, H1_135, H2_135, 135);
 
-	// Calculate first- and second-order derivatives
-	for (int i = 2; i < img.rows-2; ++i){
-		for (int j = 4; j < img.cols-4; ++j){
-
-			for (int _i = -2; _i <= 2; ++_i){
-				for (int _j = -4; _j <= +4; ++_j){
-						I_der[i][j]  += static_cast<int>(img.at<uchar>(i +_i, j + _j)) * H1[_i+2][_j+4];
-						I_der2[i][j] += static_cast<int>(img.at<uchar>(i +_i, j + _j)) * H2[_i+2][_j+4];
-					}
-			}
-		}
-	}
-
-	//--- temp log
-	ofstream f2;
-	f2.open("der.txt");
-
-
-	int c = 1;
-	for (int i = 0; i < 128; ++i){
-		for (int j = 0; j < 128; ++j){
-			f2 << I_der2[i][j] << "," << c << endl;
-			c++;
-		}
-		
-	}
-
-	// Create binary image, this will contain the extracted principal lines
-	Mat binaryImage(this->roi.size(), CV_8U);
-
-	for (int i = 1; i < 126; ++i){
-		for (int j = 3; j < 124; ++j){
-			if (((I_der[i][j] * I_der[i][j - 3]) < 0 || (I_der[i][j] * I_der[i][j - 2]) < 0 ||
-				(I_der[i][j] * I_der[i][j - 1])  < 0 || I_der[i][j] == 0) && I_der2[i][j] > 10){
-				binaryImage.at<uchar>(i, j) = 255;
-			}
-			else{
-				binaryImage.at<uchar>(i, j) = 0;
-			}
-		}
-	}
+	// Merging the principal lines with an OR operation
+	Mat finalLines = linesInDir0 | linesInDir90 | linesInDir45 | linesInDir135;
 	
-	f2.close();
 	namedWindow("Lines", CV_WINDOW_AUTOSIZE);
-	imshow("Lines", binaryImage);
+	imshow("Lines", finalLines);
+}
 
+bool PrincipalLineExtraction::isInsideTheBoundary(int i, int j){
+	if (i > 0 && i < roiHeight && j > 0 && j < roiWidth){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 PrincipalLineExtraction::~PrincipalLineExtraction()
--- cplusplus_palmPrintAuthentication/PalmPrintAuthentication/PalmPrintAuthentication/PrincipalLineExtraction.h
+++ cplusplus_palmPrintAuthentication/PalmPrintAuthentication/PalmPrintAuthentication/PrincipalLineExtraction.h
@@ -11,7 +11,7 @@ using namespace cv;
 
 /*
 	This class extracts the principal lines from the ROI (output of the RoiExtraction phase)
-	and stores it using chain codes
+	and stores it, using chain codes
 */
 class PrincipalLineExtraction
 {
@@ -26,11 +26,26 @@ private:
 	/* Input image, this is the output of the RoiExtraction phase*/
 	Mat roi;
 
-	/* Normalizes the image */
+	/* Defines the range of the first-order derivative's change detection*/
+	int firstDerivChangeWidth = 4;
+
+	/* Defines the second-order derivative's treshold value*/
+	int secondDerivTresholdValue = 10;
+	
+	/* Defines the ROI's width and height in this phase*/
+	static const int roiWidth  = 128;
+	static const int roiHeight = 128;
+
+	/* Normalizes the image  (using Histogram Equalization and a Low-pass Filter)*/
 	void normalizeImage(Mat &img);
 
-	/* Locate principal lines in four directions (0, 45, 90, 135), then merge them, 
-	  and apply morphological operations to correct these lines*/
-	void locatePrincipalLines(Mat &img);
+	/* Checks if the given coordinate is inside the image*/
+	bool isInsideTheBoundary(int i, int j);
+
+	/* Locates principal lines in a given direction*/
+	Mat locatePrincipalLineInGivenDirection(const Mat &img, std::vector<std::vector<double>> H1, std::vector<std::vector<double>> H2, int degree);
+
+	/* Locates the final principal lines (contains the merged lines) */
+	void locatePrincipalLines();
 };
 
